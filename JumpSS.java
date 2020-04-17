5fd9e93120e0480f2cc5f791532b1f3a26b0e477
dshah7577 <darpan2221@gmail.com>
2020-04-17 18:57:46 +0530

    Initial commit

---
+++ JumpSS.java
@@ -0,0 +1,20 @@
+class JumpSS
+{
+	public static void main (String [] arg)
+	{
+		for (int i=0;i<5;i++)
+		{
+			for (int j=0;j<i;j++)
+		{
+			for (int k=0;k<5;k++)
+		{
+			System.out.print(k);
+		}
+			System.out.print(j);
+		}
+			System.out.println(" ");
+
+		}
+		System.out.println("Hello");
+	}
+}
